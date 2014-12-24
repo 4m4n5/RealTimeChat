780d0378ce9e31b7f06048c8a59e991113f0ef7b
Aman Shrivastava <aman.srivastava999@gmail.com>
2014-12-24 18:02:49 +0530

    Added public directory

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ var express = require("express");
 var app = express();
 var port = 3700;
 
+//informing express to use jade
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.get("/", function(req, res){
+    res.render("page");
+});
+
+//sending response
 app.get("/", function(req, res){
     res.send("It Works!");
 });
 
+//look for static resources
+app.use(express.static(__dirname + '/public'));
+
+//listening to port
 app.listen(port);
 console.log("Listening on port " + port);
