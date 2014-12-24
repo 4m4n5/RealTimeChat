02cdea999142bc59c565ff601a7deed0656b7e5b
Aman Shrivastava <aman.srivastava999@gmail.com>
2014-12-24 20:04:20 +0530

    Added jQuery for listening to enter key

--- public/chat.js
+++ public/chat.js
@@ -5,23 +5,41 @@ window.onload = function() {
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
+    var name = document.getElementById("name");
  
     socket.on('message', function (data) {
         if(data.message) {
-            messages.push(data.message);
+            messages.push(data);
             var html = '';
             for(var i=0; i<messages.length; i++) {
-                html += messages[i] + '<br>';
+                html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
+                html += messages[i].message + '<br>';
             }
             content.innerHTML = html;
+            content.scrollTop = content.scrollHeight;
         } else {
             console.log("There is a problem:", data);
         }
     });
+    
+    //jQuery for listening to enter keypress
+    $(document).ready(function() {
+        $("#field").keyup(function(e) {
+            if(e.keyCode == 13) {
+                sendMessage();
+            }
+        });
+    });
  
-    sendButton.onclick = function() {
-        var text = field.value;
-        socket.emit('send', { message: text });
+    sendButton.onclick = sendMessage = function() {
+        if(name.value == ""){
+            alert("Please type your name! Douchebag");
+        }
+        else {
+            var text = field.value;
+            socket.emit('send', { message: text, username: name.value });
+            field.value = "";
+        }
     };
  
 }
