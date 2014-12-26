0c59e6452447c25d1288d414eddfb532711c7051
Aman Shrivastava <aman.srivastava999@gmail.com>
2014-12-26 16:35:24 +0530

    Connecting to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
+var mongo = require('mongodb').MongoClient;
 
 //informing express to use jade
 app.set('views', __dirname + '/tpl');
@@ -25,23 +26,29 @@ app.use(express.static(__dirname + '/public'));
 var usernames = {};
 var numUsers = 0;
 
+mongo.connect('mongodb://aman:thermo999@ds063870.mongolab.com:63870/chat', function(err, db){
+    if (err) throw err;
+
+
 //starting socket.io integration
-var io = require('socket.io').listen(app.listen(port));
+    var io = require('socket.io').listen(app.listen(port));
 
-//recieving message from client and sending to others
-io.sockets.on('connection', function (socket) {
-    numUsers += 1;
-    socket.emit('userNum',{ userNum: numUsers });
-    socket.broadcast.emit('userNum',{ userNum: numUsers });
-    socket.emit('message', { message: 'Welcome to Raven! A real time web chat engine.' });
-    socket.on('send', function (data) {
-        io.sockets.emit('message', data);
+    //recieving message from client and sending to others
+    io.sockets.on('connection', function (socket) {
+        numUsers += 1;
+        socket.emit('userNum',{ userNum: numUsers });
+        socket.broadcast.emit('userNum',{ userNum: numUsers });
+        socket.emit('message', { message: 'Welcome to Raven! A real time web chat engine.' });
+        socket.on('send', function (data) {
+            io.sockets.emit('message', data);
+        });
+
+        socket.on('disconnect', function(){
+            numUsers -= 1;
+            socket.broadcast.emit('userNum', { userNum: numUsers });
+        });
     });
+
+    console.log("Listening on port " + port);
     
-    socket.on('disconnect', function(){
-        numUsers -= 1;
-        socket.broadcast.emit('userNum', { userNum: numUsers });
-    });
 });
-
-console.log("Listening on port " + port);
