2115d13147e5330853f6388194a6ae88f88b04d3
Aman Shrivastava <aman.srivastava999@gmail.com>
2014-12-26 17:14:34 +0530

    fixing mongo integration

--- index.js
+++ index.js
@@ -37,8 +37,7 @@ mongo.connect('mongodb://aman:thermo999@ds063870.mongolab.com:63870/chat', funct
         
         //showing older messages
         var col = db.collection('messages');
-        col.find().limit(100).sort({_id: 1});
-        socket.emit('message', col);
+        socket.emit('message', col.find().limit(100).sort({_id: 1}));
         
         
         
