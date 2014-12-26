e6232ec0a967b75afb1afd419839047359a93c80
Aman Shrivastava <aman.srivastava999@gmail.com>
2014-12-27 00:18:46 +0530

    Fixing the colon bug

--- public/chat.js
+++ public/chat.js
@@ -27,7 +27,7 @@ window.onload = function() {
             messages.push(data);
             var html = '';
             for(var i=0; i<messages.length; i++) {
-                if (i !== 0 && messages[i].name[messages[i].name.length - 1] != ':') {
+                if (i !== 0 && messages[i].name[messages[i].name.length - 2] != ':') {
 					messages[i].name += ': ';
 				}
                 html += '<p class="chat-msg"><strong>' + (messages[i].name ? messages[i].name : '') + '</strong>';
