4184b664afd31154824051524b1eaed4c65cdd5d
Aman Shrivastava <aman.srivastava999@gmail.com>
2015-01-05 03:03:25 +0530

    emoji integration self #3

--- public/chat.js
+++ public/chat.js
@@ -96,7 +96,7 @@ window.onload = function() {
     //emoji integration
 //    emojify.setConfig({img_dir:'/pngs'});
 //    emojify.run(document.getElementById('chat-msg'));
-        function addImage ('img') {
+    var addImage = function(img) {
         var initialhtml = field.value;
         var finalhtml = field.value += '<img src= "pngs/+1.png">';
         field.value = finalhtml;
